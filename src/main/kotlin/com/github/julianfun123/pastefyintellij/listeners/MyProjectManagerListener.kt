6fa63ad8e42609de2d7d7bd06ae071af26e1b187
GitHub Action <action@github.com>
2022-06-25 20:06:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/julianfun123/pastefyintellij/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/julianfun123/pastefyintellij/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.julianfun123.pastefyintellij.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.julianfun123.pastefyintellij.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/julianfun123/pastefyintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.julianfun123.pastefyintellij.services
+
+import com.github.julianfun123.pastefyintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
